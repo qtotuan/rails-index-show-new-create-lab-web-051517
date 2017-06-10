051c07d1aa31c5a0a3e0ff72b04c53fa3f9874e4
qtotuan <quynh.totuan@gmail.com>
2017-06-10 09:45:43 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+coupons = [
+  {coupon_code: "NYCFREE", store: "MTA"},
+  {coupon_code: "FREESTUFF", store: "Only"},
+  {coupon_code: "SUPERFREE", store: "Target"}
+]
+
+Coupon.create(coupons)
